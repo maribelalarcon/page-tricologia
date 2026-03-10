3a9ca2f3960a1a22a6da0282b55bfc95264641a4
Maribel <maribelsoledadalarcon@gmail.com>
2026-03-10 17:58:54 +0100

    Delete about me and fix:footer

--- src/Data/blogData.js
+++ src/Data/blogData.js
@@ -6,8 +6,6 @@ export const blogPosts = [
       "Consiste en la aplicación de minoxidil directamente en el cuero cabelludo mediante microinyecciones. Este tratamiento ayuda a estimular el crecimiento del cabello, fortalecer los folículos pilosos y prolongar la fase de crecimiento capilar, logrando un cabello más fuerte y denso.",
     image: "/treatments/minoxidil.jpg",
     slug: "lorem-ipsum-1",
-    date: "October 23, 2025",
-    author: "John Doe",
     content: `
       <p>Consiste en la aplicación de minoxidil directamente en el cuero cabelludo mediante microinyecciones. Este tratamiento ayuda a estimular el crecimiento del cabello, fortalecer los folículos pilosos y prolongar la fase de crecimiento capilar, logrando un cabello más fuerte y denso.</p>
     `,
@@ -19,8 +17,6 @@ export const blogPosts = [
       "Indicada principalmente en alopecias inflamatorias o de origen autoinmune. Su función es reducir la inflamación del cuero cabelludo, aliviar síntomas como picazón o enrojecimiento y ayudar a frenar la progresión de la caída del cabello.",
     image: "/treatments/corticoides.jpg",
     slug: "lorem-ipsum-2",
-    date: "October 10, 2025",
-    author: "Jane Smith",
     content: `
       <p>Indicada principalmente en alopecias inflamatorias o de origen autoinmune. Su función es reducir la inflamación del cuero cabelludo, aliviar síntomas como picazón o enrojecimiento y ayudar a frenar la progresión de la caída del cabello.</p>
     `,
@@ -32,8 +28,6 @@ export const blogPosts = [
       "Recomendada en casos de alopecia androgenética (hereditaria u hormonal). Actúa bloqueando o reduciendo el efecto de las hormonas androgénicas sobre el folículo piloso, ayudando a frenar la miniaturización del cabello y a preservar su densidad.",
     image: "/treatments/antiandrogenos.jpg",
     slug: "lorem-ipsum-3",
-    date: "October 2, 2025",
-    author: "Alice Brown",
     content: `
       <p>Recomendada en casos de alopecia androgenética (hereditaria u hormonal). Actúa bloqueando o reduciendo el efecto de las hormonas androgénicas sobre el folículo piloso, ayudando a frenar la miniaturización del cabello y a preservar su densidad.</p>
     `,
@@ -45,8 +39,6 @@ export const blogPosts = [
       "Tratamiento revitalizante que combina vitaminas, minerales, aminoácidos y otros activos que mejoran la microcirculación del cuero cabelludo, fortalecen el cabello existente y estimulan el crecimiento de nuevos cabellos.",
     image: "/treatments/estimulante.jpg",
     slug: "lorem-ipsum-4",
-    date: "September 30, 2025",
-    author: "John Doe",
     content: `
       <p>Tratamiento revitalizante que combina vitaminas, minerales, aminoácidos y otros activos que mejoran la microcirculación del cuero cabelludo, fortalecen el cabello existente y estimulan el crecimiento de nuevos cabellos.</p>
     `,
